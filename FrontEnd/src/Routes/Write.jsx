0b9abb2a46d5f5a896e547c33a47aa890240e442
Rohan <timalsinarohan9@gmail.com>
2025-10-26 21:49:08 +0545

    mm

--- FrontEnd/src/Routes/Write.jsx
+++ FrontEnd/src/Routes/Write.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useUser } from "@clerk/clerk-react";
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 export const Write = () => {
   const { isSignedIn, isLoaded } = useUser();
@@ -10,14 +12,14 @@ export const Write = () => {
     return <div>You should loged In</div>
   }
   return (
-    <div>
-      <h1>Create a New Post 
-        <form action="">
-          <button>Add a cover image</button>
-          <input type="text" placeholder='My Awesome Story ' />
-          <div className="">
-            <label htmlFor="">Choose a category: </label>
-              <select name="cat" id="">
+    <div className='h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6'>
+      <h1 className='text-xl font-light'>Create a New Post </h1>
+        <form action="" className='flex flex-col gap-6 flex-1 mb-6'>
+          <button className='p-2 w-max shadow-md rounded-xl text-sm text-gray-500 bg-white'>Add a cover image</button>
+          <input className='text-4xl font-semibold bg-transparent outline-none' type="text" placeholder='My Awesome Story ' />
+          <div className="flex items-center gap-4">
+            <label htmlFor="" className='text-sm'>Choose a category: </label>
+              <select name="cat" id="" className='p-2 rounded-xl bg-white shadow-md'>
                 <option value="general">General</option>
                 <option value="web-desgin">Web Design</option>
                 <option value="development">Development</option>
@@ -27,9 +29,10 @@ export const Write = () => {
               </select>
             
           </div>
-          <textarea name="" placeholder='A Short Description' id=""/>
+          <textarea className='p-4 rounded-xl bg-white shadow-md' name="" placeholder='A Short Description' id="" />
+          <ReactQuill className='flex-1  rounded-xl bg-white shadow-md' theme="snow" />
+          <button className='bg-[#540000] mt-4 p-2 w-36 text-white font-medium rounded-xl'>Send</button>
       </form>
-      </h1>
     </div>
   )
 }
