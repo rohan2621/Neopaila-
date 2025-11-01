c2b940b58e4dd1055272032335b74ef473103b06
Rohan <timalsinarohan9@gmail.com>
2025-11-01 22:24:09 +0545

    o

--- FrontEnd/src/Components/SideMenu.jsx
+++ FrontEnd/src/Components/SideMenu.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Search } from './Search';
+import { Link } from 'react-router';
 
 const SideMenu = () => {
   return (
-    <div>SideMenu</div>
+    <div className='px-4 h-max sticky'>
+      <h1 className='mb-4 text-sm font-medium'>Search</h1>
+      <Search/>
+      <h1 className='mb-4 text-sm font-medium'>Filter</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'></label>
+      </div>
+      <h1 className='mb-4 text-sm font-medium'>Categories</h1>
+      <div className='flex flex-col gap-2 text-sm'>
+        <Link className='underline' to={'/posts'}>All</Link>
+        <Link className='underline' to={'/posts?cat=web-design'}>Web Design</Link>
+        <Link className='underline' to={'/posts?cat=development'}>Development</Link>
+        <Link className='underline' to={'/posts?cat=databases'}>Databases</Link>
+        <Link className='underline' to={'/posts?cat=seo'}>Search Engine</Link>
+        <Link className='underline' to={'/posts?cat=marketing'}>Marketing</Link>
+        
+      </div>
+    </div>
   )
 }
 
