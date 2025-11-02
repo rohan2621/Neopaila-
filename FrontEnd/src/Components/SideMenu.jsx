aea0b0bc0d96994185a303a6f21b3549b2aedb00
Rohan <timalsinarohan9@gmail.com>
2025-11-02 22:49:35 +0545

    backend

--- FrontEnd/src/Components/SideMenu.jsx
+++ FrontEnd/src/Components/SideMenu.jsx
@@ -6,10 +6,23 @@ const SideMenu = () => {
   return (
     <div className='px-4 h-max sticky'>
       <h1 className='mb-4 text-sm font-medium'>Search</h1>
-      <Search/>
+      <Search />
       <h1 className='mb-4 text-sm font-medium'>Filter</h1>
       <div className="flex flex-col gap-2 text-sm">
-        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'></label>
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input type="radio" name='sort' value={'newest'} className='appearance-none bg-white w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800' />
+          Newest
+        </label><label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input type="radio" name='sort' value={'popular'} className='appearance-none bg-white w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800' />
+          Most Popular
+        </label><label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input type="radio" name='sort' value={'trending'} className='appearance-none bg-white w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800' />
+          Trending
+        </label>
+        <label htmlFor="" className='flex items-center gap-2 cursor-pointer'>
+          <input type="radio" name='sort' value={'oldest'} className='appearance-none bg-white w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800' />
+          Oldest
+        </label>
       </div>
       <h1 className='mb-4 text-sm font-medium'>Categories</h1>
       <div className='flex flex-col gap-2 text-sm'>
@@ -19,7 +32,7 @@ const SideMenu = () => {
         <Link className='underline' to={'/posts?cat=databases'}>Databases</Link>
         <Link className='underline' to={'/posts?cat=seo'}>Search Engine</Link>
         <Link className='underline' to={'/posts?cat=marketing'}>Marketing</Link>
-        
+
       </div>
     </div>
   )
