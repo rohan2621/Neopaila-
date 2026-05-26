49ef093b0988690e50b030a3d36b1c3ccbc466d1
Rohan Timalsina <timalsinarohan9@gmail.com>
2026-05-26 16:56:14 +0545

    Update LoginPage.jsx

--- FrontEnd/src/Routes/LoginPage.jsx
+++ FrontEnd/src/Routes/LoginPage.jsx
@@ -8,18 +8,18 @@ const LoginPage = () => {
     gsap.from(".login", {
       scale: 0,
       opacity: 0,
-
       duration: 0.7,
       ease: "power1.inOut",
     });
   }, []);
+
   return (
     <div className="flex login px-2 md:px-4 lg:px-8 xl:px-16 2xl:px-32 items-center justify-center h-[calc(100vh-80px)]">
-     <SignIn
-  routing="hash"
-  fallbackRedirectUrl="/continue"
-  signUpForceRedirectUrl="/continue"
-/>
+      <SignIn
+        routing="path"
+        fallbackRedirectUrl="/"
+        signUpForceRedirectUrl="/"
+      />
     </div>
   );
 };
